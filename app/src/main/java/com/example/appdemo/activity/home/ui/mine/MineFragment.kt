40bc4dd26ba20d4baa1b56fa6e49084587c924cc
wqqDuan <726395234@qq.com>
2024-04-06 01:31:05 +0800

    [feat] FlutterFragment引入！！

--- app/src/main/java/com/example/appdemo/activity/home/ui/mine/MineFragment.kt
+++ app/src/main/java/com/example/appdemo/activity/home/ui/mine/MineFragment.kt
@@ -4,25 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.height
-import androidx.compose.material.Text
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.platform.ViewCompositionStrategy
-import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
-import com.example.appdemo.activity.FragmentContainer
-import com.example.appdemo.flutter.FlutterRootFragment
-import com.example.appdemo.flutter.FlutterRuntimeUtil
-import io.flutter.embedding.android.FlutterFragment
+import com.example.appdemo.R
+import com.idlefish.flutterboost.containers.FlutterBoostFragment
 
 class MineFragment : Fragment() {
 
-    private var flutterFragment: FlutterFragment? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        acquireFlutterFragment()
     }
 
     override fun onCreateView(
@@ -30,30 +19,13 @@ class MineFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        acquireFlutterFragment()
-        return ComposeView(requireContext()).apply {
-            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
-            setContent {
-                Text(text = "MineTab")
-//                FragmentContainer(
-//                    modifier = Modifier.height(400.dp),
-//                    fragment = flutterFragment!!,
-//                    fragmentManager = requireActivity().supportFragmentManager,
-//                    commit =  { replace(it, flutterFragment!!) }
-//                )
-            }
-        }
+        val mView = inflater.inflate(R.layout.fragment_mine, null)
+        val flutterFragment = FlutterBoostFragment.CachedEngineFragmentBuilder()
+            .url("flutter://demo1")
+            .build<FlutterBoostFragment>()
+        parentFragmentManager.beginTransaction()
+            .add(R.id.mine_flutter_fragment, flutterFragment).commitAllowingStateLoss()
+        return mView
     }
 
-    private fun acquireFlutterFragment() {
-        if (flutterFragment != null) {
-            return
-        }
-        flutterFragment = FlutterRootFragment
-            .withCachedEngine(FlutterRuntimeUtil.DEFAULT_FLUTTER_ENGINE).build<FlutterFragment>()
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-    }
 }
